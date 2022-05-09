b8e834faf4cb391a15d5ba7145647efba47ca75d
JhonDCT <deyner.catacora@gmail.com>
2022-05-08 21:14:31 -0500

    youtube downloader client

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const handler = require("./handler");
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.get("/download", async (req, res) => {
   const response = await handler.download({ url: req.query.url });
