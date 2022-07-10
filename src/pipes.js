e05497a3c4b0ce65d49fcb7fea86acd956b9e7ed
JhonDCT <deyner.catacora@gmail.com>
2022-07-09 19:28:39 -0500

    fix (downloader): restore delete pipe function

--- src/pipes.js
+++ src/pipes.js
@@ -11,4 +11,23 @@ module.exports = class Pipes {
       path,
     };
   }
+
+  /**
+   * @summary Create a pipe for get info
+   * @param {*} data
+   * @return {*}
+   */
+  static getInfo(data) {
+    const { videoDetails, formats } = data;
+
+    const info = this.details(videoDetails);
+    const availableFormats = formats
+      .filter((item) => item.hasAudio)
+      .map((item) => this.availableFormats(item));
+
+    return {
+      info,
+      availableFormats,
+    };
+  }
 };
