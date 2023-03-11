a42af6e15a3916ddd8dd85ed71d66723193c2f81
JhonDCT <deyner.catacora@gmail.com>
2023-03-11 13:10:23 -0500

    fix: download route

--- sh.deploy.sh
+++ sh.deploy.sh
@@ -12,9 +12,9 @@ echo $branch_name
 git switch $branch_name
 git pull $branch_name
 
-sudo docker stop $(sudo docker ps | grep yt-downloader | awk '{print $1}' | head -1)
-sudo docker rm $(sudo docker ps -a | grep yt-downloader | awk '{print $1}' | head -1)
-sudo docker rmi yt-downloader
+docker stop $(docker ps | grep yt-downloader | awk '{print $1}' | head -1)
+docker rm $(docker ps -a | grep yt-downloader | awk '{print $1}' | head -1)
+docker rmi yt-downloader
 
-sudo docker build . -t yt-downloader
-sudo docker run -p 3000:3000 -d yt-downloader
+docker build . -t yt-downloader
+docker run -p 3000:3000 -d yt-downloader
